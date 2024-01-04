ee976b41f3a2642855a73078005cae6df3e68b08
yuliiasavitska <yuliia.savitska.gr@gmail.com>
2024-01-04 20:57:42 +0100

    Refactor color

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:user_list/list_of_users/list_of_users_page.dart';
 import 'package:user_list/providers/users_provider.dart';
+import 'package:user_list/widgets/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'List of users',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: activeGreenColor),
         useMaterial3: true,
       ),
       home: ChangeNotifierProvider(
